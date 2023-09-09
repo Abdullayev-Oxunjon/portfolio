d455430585181bef3c12a96bf750d1fb08a21bc9
Abdullayev Oxunjon <143160799+Abdullayev-Oxunjon@users.noreply.github.com>
2023-09-09 07:35:00 +0500

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from app.models import Home, About, Contact, SocialNetwork, Experience, Skills, Services, Portfolio, Blog, Visit
+from app.models import Home, About, Contact, SocialNetwork, Experience, Skills, Services, Portfolio, Blog, Visit, ForPdf
 
 admin.site.register([Home, About, SocialNetwork, Experience,
-                     Skills, Services, Portfolio, Blog, Contact, Visit])
+                     Skills, Services, Portfolio, Blog, Contact, Visit,ForPdf])
